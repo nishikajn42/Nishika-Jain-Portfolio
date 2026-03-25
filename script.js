0f807f3cb0a4e506c6d85e546b9cb87251a2e9e0
nishikajn42 <modinishika1542@gmail.com>
2026-03-25 09:11:43 +0530

    add file

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
-var t1 = gsap.timeline();
-t1.from("header", {
-  y: -100,
-  opacity: 0,
-  duration: 0.5,
-  stagger: 0.4,
-});
-t1.from(".iam", {
-  x: -500,
-  opacity: 0,
-  duration: 2,
-});
-t1.from(".nishikajain", {
-  x: 1500,
-  opacity: 0,
-  duration: 2,
-});
-t1.from(".circlebg img", {
-  opacity: 0,
-  duration: 1,
-});
-const scroll = new LocomotiveScroll({
-  el: document.querySelector("[data-scroll-container]"),
-  smooth: true,
+// Register the scroll plugin
+gsap.registerPlugin(ScrollTrigger);
+
+// 1. Initial Loading Sequence (Timeline)
+const tl = gsap.timeline();
+
+tl.fromTo(".iam", 
+    { x: -100, opacity: 0 }, 
+    { x: 0, opacity: 1, duration: 0.8, ease: "power3.out" }
+)
+.fromTo(".first-name", 
+    { x: -200, opacity: 0 }, 
+    { x: 0, opacity: 1, duration: 0.8, ease: "power3.out" }, 
+    "-=0.5" // Starts 0.5s before previous ends
+)
+.fromTo(".last-name", 
+    { x: 200, opacity: 0 }, 
+    { x: 0, opacity: 1, duration: 0.8, ease: "power3.out" }, 
+    "-=0.8"
+)
+.fromTo(".circlebg img", 
+    { opacity: 0, scale: 0.8 }, 
+    { opacity: 1, scale: 1, duration: 1.2, ease: "back.out(1.7)" }
+);
+
+// 2. Scroll Animation (Move image right when scrolling down)
+gsap.to(".circlebg img", {
+    scrollTrigger: {
+        trigger: ".bg",      // Starts when the first page is active
+        start: "top top",    // When top of page hits top of viewport
+        end: "bottom top",   // Ends when bottom of page hits top of viewport
+        scrub: 1,            // Ties animation to scroll speed
+        scrub: 1,            // Smoothly ties movement to your finger/wheel
+        toggleActions: "restart none none reverse"
+    },
+    x: "60vw",               // Moves the image significantly to the right
+    ease: "none"
 });
 
